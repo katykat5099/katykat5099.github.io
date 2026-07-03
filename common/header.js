19efffcec0c65366fdbb1bfb280faa26206727fd
Katy (Tabletop-Windows) <katykat5099@gmail.com>
2026-07-03 17:09:02 -0500

    Replace Home button with Title

--- common/header.js
+++ common/header.js
@@ -5,10 +5,9 @@
     const fallbackHeader = `
 <div class="titlehead"> <!-- This "titlehead" class is used for modifying the entire head section -->
     <div class="header"> <!-- This "header" class is used for the title or "logo" -->
-        <h1>Katy's Projects</h1>
+        <a href="index.html" data-root-href="index.html" class="title-link"><h1>Katy's Projects</h1></a>
     </div>
     <div class="explore"> <!-- This class "explore" is for the buttons -->
-        <a href="index.html" data-root-href="index.html" class="headerbutton">Home</a>
         <a href="minecraft/minecraft_projects.html" data-root-href="minecraft/minecraft_projects.html" class="headerbutton">Minecraft Projects</a>
         <a href="mystie_forest/mystie_forest.html" data-root-href="mystie_forest/mystie_forest.html" class="headerbutton">Mystie Forest</a>
     </div>
