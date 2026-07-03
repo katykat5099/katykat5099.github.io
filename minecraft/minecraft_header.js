add8143d80b90614c00e6f9ea43e4bbb404c6215
Katy (Tabletop-Windows) <katykat5099@gmail.com>
2026-07-03 17:26:37 -0500

    Added Nav Bar in Minecraft Projects

---
+++ minecraft/minecraft_header.js
@@ -0,0 +1,33 @@
+(function () {
+    const currentScript = document.currentScript;
+    const minecraftRoot = currentScript ? currentScript.dataset.minecraftRoot || "" : "";
+    const container = document.getElementById("minecraft-header-container");
+    const fallbackHeader = `
+<div>
+  <h1 class="welcomer">Minecraft Projects</h1>
+  <div class="buttonswrap">
+    <a href="alphaworld/alphaworld1.html" data-minecraft-href="alphaworld/alphaworld1.html" class="minecraftbutton">ALPHA WORLD</a>
+    <a href="elyxion/elyxion.html" data-minecraft-href="elyxion/elyxion.html" class="minecraftbutton">Elyxion</a>
+  </div>
+  <div class="spacing">
+    <hr class="divider" />
+  </div>
+</div>`;
+
+    if (!container) {
+        return;
+    }
+
+    function applyHeader(html) {
+        container.innerHTML = html;
+
+        container.querySelectorAll("[data-minecraft-href]").forEach((link) => {
+            link.setAttribute("href", `${minecraftRoot}${link.dataset.minecraftHref}`);
+        });
+    }
+
+    fetch(`${minecraftRoot}minecraft_header.html`)
+        .then((response) => response.ok ? response.text() : fallbackHeader)
+        .then(applyHeader)
+        .catch(() => applyHeader(fallbackHeader));
+})();
