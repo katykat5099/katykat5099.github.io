1682b674a2f587d62643f61b34260ca1e0382582
Katy (Tabletop-Windows) <katykat5099@gmail.com>
2026-07-04 13:17:56 -0500

    Created a list for versions instead of hard coding it into minehistory Also added Classic version page

---
+++ minecraft/history/history-data.js
@@ -0,0 +1,42 @@
+window.mineHistoryData = {
+  versions: [
+    { name: "Beta 1.7.2", href: "versions/B1.7.2.html" },
+    { name: "Alpha 1.2.6", href: "versions/A1.2.6.html" },
+    { name: "Beta 1.9_pre1", href: "versions/B1.9_pre1.html" },
+    { name: "Beta 1.3_01", href: "versions/B1.3_01.html" },
+    { name: "Beta 1.8_pre2", href: "versions/B1.8_pre2.html" },
+    { name: "Beta 1.6.6", href: "versions/B1.6.6.html" },
+    { name: "Beta 1.9_pre3", href: "versions/B1.9_pre3.html" },
+    { name: "Beta 1.2", href: "versions/B1.2.html" },
+    { name: "Beta 1.5_02", href: "versions/B1.5_02.html" },
+    { name: "Alpha 1.1.2_01", href: "versions/A1.1.2_01.html" },
+    { name: "Beta 1.8", href: "versions/B1.8.html" },
+    { name: "Beta 1.4_01", href: "versions/B1.4_01.html" },
+    { name: "Beta 1.9_pre2", href: "versions/B1.9_pre2.html" },
+    { name: "Beta 1.6.1", href: "versions/B1.6.1.html" },
+    { name: "Beta 1.8_pre1", href: "versions/B1.8_pre1.html" },
+    { name: "Classic 0.0.11a", href: "versions/C0.0.11a.html" },
+  ],
+  mods: [
+    {
+      name: "The Aether",
+      href: "mods/the_aether/the_aether.html",
+    },
+    {
+      name: "IndustrialCraft 2",
+      href: "mods/industrial_craft_2/industrial_craft_2.html",
+    },
+    {
+      name: "Equivalent Exchange",
+      href: "mods/equivalent_exchange/equivalent_exchange.html",
+    },
+    {
+      name: "TooManyItems",
+      href: "mods/toomanyitems/toomanyitems.html",
+    },
+    {
+      name: "Battle Towers",
+      href: "mods/battle_towers/battle_towers.html",
+    },
+  ],
+};
